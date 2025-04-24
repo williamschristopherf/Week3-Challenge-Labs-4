281a5601550cf9dec0ee6db5c9b89d228fbcd17e
Christopher Williams <williamstophef@gmail.com>
2025-04-24 11:29:40 -0700

    REcursion version of the coded added and tested

--- Program.cs
+++ Program.cs
@@ -3,25 +3,36 @@ string sub1 = "AB";
 string sub2 = "CD";
 
 Console.WriteLine($"Initial string: {example}");
+//recursion version
 static string FindSubString(string example, string sub1, string sub2)
 {
-    do
+    string updated = example.Replace(sub1, "").Replace(sub2, "");
+    if (updated == example)
     {
-        //uses 2 .replace together and replaces with value"" which is empty 
-        string updated = example.Replace(sub1, "").Replace(sub2, "");
-        //checks for changes. If not breaks loop and outputs example.
-        if (updated == example)
-        {
-            break;
-        }
-        example = updated;
-
+        return example;
     }
-    while (true);
-
-    return example;
+    return FindSubString(updated, sub1, sub2);
 }
-
 
 Console.WriteLine($"The final substring is {FindSubString(example, sub1, sub2)}.");
 Console.WriteLine($"The resulting length of the string is {FindSubString(example, sub1, sub2).Length}.");
+
+//manual loop for  comparing substrings
+//static string FindSubString(string example, string sub1, string sub2)
+//{
+//    do
+//    {
+//        //uses 2 .replace together and replaces with value"" which is empty 
+//        string updated = example.Replace(sub1, "").Replace(sub2, "");
+//        //checks for changes. If not breaks loop and outputs example.
+//        if (updated == example)
+//        {
+//            break;
+//        }
+//        example = updated;
+
+//    }
+//    while (true);
+
+//    return example;
+//}
